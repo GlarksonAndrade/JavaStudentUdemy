1cc12e79a03592ec23995154a9e5fc4e10f5909b
Aloisio Glarkson Andrade de Jesus <aloisio.gajesus@saude.local>
2023-11-18 15:58:13 -0300

    Initial commit

---
+++ src/DesafioCalculadora.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class DesafioCalculadora {
+    public static void main(String[] args) {
+        // Ler Num1
+        // Ler num2
+        // + - * / %
+
+        System.out.println("Digite o Primeiro Numero:");
+        Scanner entrada = new Scanner(System.in);
+        double num1 = entrada.nextDouble();
+
+        System.out.println("Digite o Segundo Numero:");
+        double num2 = entrada.nextDouble();
+
+        System.out.println("Digite a operação:");
+        String operador = entrada.next();
+
+        // Lógica
+
+        double resultado = "+".equals(operador) ? num1 + num2 : 0;
+         resultado = "-".equals(operador) ? num1 - num2 : resultado;
+         resultado = "*".equals(operador) ? num1 * num2 : resultado;
+         resultado = "/".equals(operador) ? num1 / num2 : resultado;
+         resultado = "%".equals(operador) ? num1 % num2 : resultado;
+
+
+       System.out.printf("%.2f %s %.2f = %.2f",
+               num1, operador, num2, resultado);
+
+       entrada.close();
+
+    }
+}
--- src/TipoStringEquals.java
+++ src/TipoStringEquals.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class TipoStringEquals {
     public static void main(String[] args) {
