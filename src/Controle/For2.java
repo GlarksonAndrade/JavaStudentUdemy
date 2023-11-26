e8c5ebf6eb1fe079fd06d9821e2aa844bd926a0f
aloisio.gajesus <glarksonandrade@gmail.com>
2023-11-26 16:34:17 -0300

    new packager

---
+++ src/Controle/For2.java
@@ -0,0 +1,10 @@
+package Controle;
+
+public class For2 {
+    public static void main(String[] args) {
+
+        for (int numero = 10; numero >= 0; numero -= 2) {
+            System.out.printf("Numero = %d\n", numero);
+        }
+    }
+}
