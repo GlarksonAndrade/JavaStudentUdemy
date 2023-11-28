77f42b92088033054e7850b9c86c4b51b9d8ea47
aloisio.gajesus <glarksonandrade@gmail.com>
2023-11-28 20:00:16 -0300

    new packager

---
+++ src/Controle/Exercicios/Teste.java
@@ -0,0 +1,53 @@
+package Controle.Exercicios;
+
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("Digite o ano Inicial");
+        int anoinicial = entrada.nextInt();
+
+        System.out.println("Digite o ano Final");
+        int anofinal = entrada.nextInt();
+
+        int ano = anofinal - anoinicial;
+
+        int total = 0;
+
+        int bisexto = 0;
+        int normal = 0;
+
+        for (ano = ano; ano != 1; ano--) {
+
+            if (ebissexto(ano)) {
+                total = total+ 366;
+                bisexto++;
+            } else
+                total = total + 365;
+            normal++;
+        }
+        System.out.printf("Neste intervalo de anos temos,\n%d, anos bissextos,\n%d anos normais e um total de %d dias", bisexto, normal, total);
+
+        entrada.close();
+    }
+
+    static boolean ebissexto(int ano) {
+        if (ano % 100 != 0 && ano % 4 == 0) {
+            //System.out.printf("O ano %d, é bissexto e tem 366 dias!", ano);
+            return true;
+        } else if (ano % 400 == 0 && ano % 100 == 0) {
+            //System.out.printf("O ano %d, é bissexto e tem 366 dias!", ano);
+            return true;
+        } else {
+            //System.out.printf("O ano %d, não é um ano bissexto e tem 365 dias ", ano);
+        }
+        return false;
+
+    }
+
+}
+
+
--- src/Controle/Exercicios/bissexto.java
+++ src/Controle/Exercicios/bissexto.java
@@ -1,7 +1,27 @@
 package Controle.Exercicios;
 
+import java.util.Scanner;
+
 public class bissexto {
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digíte o ano: ");
+
+        int ano = entrada.nextInt();
+
+
+        if (ano % 100 != 0 && ano % 4 == 0) {
+            System.out.printf("O ano %d, é bissexto e tem 366 dias!", ano);
+        }
+            else if (ano % 400 == 0 && ano % 100 == 0) {
+            System.out.printf("O ano %d, é bissexto e tem 366 dias!", ano);
+        }
+       else
+        System.out.printf("O ano %d, não é um ano bissexto e tem 365 dias ", ano);
+
+        entrada.close();
 
     }
 }
