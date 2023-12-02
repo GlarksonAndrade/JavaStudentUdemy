46783fb62e3243bb7923e43bea08ed4bacd216e4
aloisio.gajesus <glarksonandrade@gmail.com>
2023-12-02 18:59:57 -0300

    new packager

---
+++ src/Classe/Data.java
@@ -0,0 +1,8 @@
+package Classe;
+
+public class Data {
+
+    int dia;
+    int mes;
+    int ano;
+}
---
+++ src/Classe/Produto.java
@@ -0,0 +1,8 @@
+package Classe;
+
+public class Produto {
+
+    String nome;
+    double preco;
+    double desconto;
+}
