ed23a8d35bc029c4ef6c0dc76f13c756b6b4b442
aloisio.gajesus <glarksonandrade@gmail.com>
2024-01-07 16:32:19 -0300

    new packager

---
+++ src/colecoes/Fila.java
@@ -0,0 +1,38 @@
+package colecoes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fila {
+    public static void main(String[] args) {
+
+        Queue<String> fila = new LinkedList<>();
+
+        fila.add("Ana");
+        fila.offer("Bia");
+        fila.add("Carlos");
+        fila.offer("Daniel");
+        fila.add("Rafaela");
+        fila.offer("Gui");
+
+        System.out.println(fila.peek());
+        System.out.println(fila.peek());
+        System.out.println(fila.element());
+        System.out.println(fila.element());
+
+        //fila.size();
+        //fila.clear();
+        //fila.isEmpty();
+        //fila.contains(.....);
+
+        System.out.println(fila.poll());
+        System.out.println(fila.remove());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.remove());
+    }
+}
