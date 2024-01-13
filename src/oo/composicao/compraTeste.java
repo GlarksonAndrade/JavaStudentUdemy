d4acedaa00e42d76f5356cd782d1342a1e0e191f
aloisio.gajesus <glarksonandrade@gmail.com>
2024-01-13 16:01:00 -0300

    new classe

---
+++ src/oo/composicao/compraTeste.java
@@ -0,0 +1,21 @@
+package oo.composicao;
+
+public class compraTeste {
+    public static void main(String[] args) {
+
+        Compra compra1 = new Compra();
+
+        compra1.cliente = "joao Pedro";
+        compra1.adicionarItem("Caneta", 20, 7.45);
+        compra1.adicionarItem(new Item("Borracha", 12, 3.89));
+        compra1.adicionarItem(new Item("Caderno", 3, 18.79));
+
+        System.out.println(compra1.itens.size());
+        System.out.println(compra1.obterValorTotal());
+
+        double total = compra1.itens.get(0).compra.itens.get(1).compra.obterValorTotal();
+
+        System.out.println("O Total é R$" + total);
+
+    }
+}
