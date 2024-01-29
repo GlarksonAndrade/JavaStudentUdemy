e55aae3251f3a0a98792d583c405fd3234dcc286
aloisio.gajesus <glarksonandrade@gmail.com>
2024-01-29 19:03:00 -0300

    atualização de class

---
+++ src/oo/Composicao/heranca/Direcao.java
@@ -0,0 +1,5 @@
+package oo.Composicao.heranca;
+
+public enum Direcao {
+    NORTE, LESTE, SUL, OESTE;
+}
--- src/oo/Composicao/heranca/Jogo.java
+++ src/oo/Composicao/heranca/Jogo.java
@@ -2,8 +2,34 @@ package oo.Composicao.heranca;
 
 public class Jogo {
     public static void main(String[] args) {
-        Jogador j1 = new Jogador(){
+        Jogador j1 = new Jogador();
+        j1.x = 10;
+        j1.y = 10;
 
-        }
+        Jogador j2 = new Jogador();
+        j2.x = 10;
+        j2.y = 11;
+
+
+//        j1.andar(Direcao.NORTE);
+//        j1.andar(Direcao.LESTE);
+//        j1.andar(Direcao.NORTE);
+//        j1.andar(Direcao.LESTE);
+
+//        System.out.println(j1.x);
+//        System.out.println(j1.y);
+
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+
+        j1.atacar(j2);
+        j2.atacar(j1);
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+
     }
 }
