da0fde68c1625b4bec883f6295a49160cc5b4d7e
aloisio.gajesus <glarksonandrade@gmail.com>
2024-01-31 18:53:35 -0300

    atualização de class

--- src/oo/Composicao/heranca/Jogo.java
+++ src/oo/Composicao/heranca/Jogo.java
@@ -2,33 +2,22 @@ package oo.Composicao.heranca;
 
 public class Jogo {
     public static void main(String[] args) {
-        Jogador j1 = new Jogador();
-        j1.x = 10;
-        j1.y = 10;
+        Monstro monstro = new Monstro();
+        monstro.x = 10;
+        monstro.y = 10;
 
-        Jogador j2 = new Jogador();
-        j2.x = 10;
-        j2.y = 11;
+        Heroi heroi  = new Heroi();
+        heroi.x = 10;
+        heroi.y = 11;
 
+        System.out.println("Monstro tem => " + monstro.vida);
+        System.out.println("Heroi tem => " + heroi.vida);
 
-//        j1.andar(Direcao.NORTE);
-//        j1.andar(Direcao.LESTE);
-//        j1.andar(Direcao.NORTE);
-//        j1.andar(Direcao.LESTE);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-//        System.out.println(j1.x);
-//        System.out.println(j1.y);
-
-
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
-
-
-        j1.atacar(j2);
-        j2.atacar(j1);
-
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        System.out.println("Monstro tem => " + monstro.vida);
+        System.out.println("Heroi tem => " + heroi.vida);
 
 
     }
