46cf0c563af7620b5c60c17c3d812be7154f4049
aloisio.gajesus <glarksonandrade@gmail.com>
2024-02-18 13:08:19 -0300

    atualização de class

--- src/lambdas/OperadorUnario.java
+++ src/lambdas/OperadorUnario.java
@@ -1,10 +1,25 @@
 package lambdas;
 
+import java.util.function.UnaryOperator;
+
 public class OperadorUnario {
     public static void main(String[] args) {
 
+        UnaryOperator<Integer> maisDOis = n -> n + 2;
+        UnaryOperator<Integer> vezesDOis = n -> n * 2;
+        UnaryOperator<Integer> aoQuadrado = n -> n * n;
+
+        int resultado1 = maisDOis
+                .andThen(vezesDOis)
+                .andThen(aoQuadrado)
+                .apply(0);
+        System.out.println(resultado1);
 
 
-
+        int resultado2 = aoQuadrado
+                .compose(vezesDOis)
+                .compose(maisDOis)
+                .apply(0);
+        System.out.println(resultado2);
     }
 }
